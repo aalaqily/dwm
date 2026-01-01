deb3e55f5725700c9eeb69735ed2c755c218dd77
Ahmad Asaad <ahmadasaadh@gmail.com>
2026-01-01 13:50:12 +0200

    refactor: replace mod key with super key

--- config.def.h
+++ config.def.h
@@ -68,13 +68,12 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define 
-#define MODKEY Mod1Mask
+#define SUPERKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ SUPERKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ SUPERKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ SUPERKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ SUPERKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -94,33 +93,33 @@ static const char *scrot[] = {"scrot", "--select", SCREENSHOTS_DIR "/%Y-%m-%d-%H
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-        { MODKEY|ShiftMask,             XK_b,      spawn,          {.v = browsercmd } },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ SUPERKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ SUPERKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ SUPERKEY,                       XK_b,      togglebar,      {0} },
+        { SUPERKEY|ShiftMask,             XK_b,      spawn,          {.v = browsercmd } },
+	{ SUPERKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ SUPERKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ SUPERKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ SUPERKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ SUPERKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ SUPERKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ SUPERKEY,                       XK_Return, zoom,           {0} },
+	{ SUPERKEY,                       XK_Tab,    view,           {0} },
+	{ SUPERKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ SUPERKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ SUPERKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ SUPERKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ SUPERKEY,                       XK_space,  setlayout,      {0} },
+	{ SUPERKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ SUPERKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ SUPERKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ SUPERKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ SUPERKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ SUPERKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ SUPERKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ SUPERKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ SUPERKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ SUPERKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -130,14 +129,14 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ SUPERKEY|ShiftMask,             XK_q,      quit,           {0} },
         { 0,                XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
 	{ 0,                XF86XK_AudioMute, spawn, {.v = mutevol } },
 	{ 0,                XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
         { 0,				XF86XK_MonBrightnessUp,		spawn,	{.v = light_up} },
 	{ 0,				XF86XK_MonBrightnessDown,	spawn,	{.v = light_down} },
-	{MODKEY,                        XK_a,      spawn,           {.v = scrot_full}},
-	{MODKEY,	 					XK_s,      spawn,           {.v = scrot}}
+	{SUPERKEY,                        XK_a,      spawn,           {.v = scrot_full}},
+	{SUPERKEY,	 					XK_s,      spawn,           {.v = scrot}}
 };
 
 /* button definitions */
@@ -148,12 +147,12 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         SUPERKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         SUPERKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         SUPERKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkTagBar,            SUPERKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            SUPERKEY,         Button3,        toggletag,      {0} },
 };
 
